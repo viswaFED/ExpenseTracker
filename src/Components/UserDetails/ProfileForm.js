8fba417979f2ee073e22e7eb0b60b2b7771d50ae
viswaFED <100114308+viswaFED@users.noreply.github.com>
2022-04-21 14:07:55 +0530

    editprofile

--- src/Components/UserDetails/ProfileForm.js
+++ src/Components/UserDetails/ProfileForm.js
@@ -1,50 +1,85 @@
-import React, { useRef } from "react";
-import './ProfileForm.css'
+import React, { useRef, useEffect } from "react";
+import "./ProfileForm.css";
 
 const ProfileForm = () => {
-    const nameInputRef= useRef();
-    const photoUrlRef  = useRef();
-    const updateHandler = async(event)=>{
+  const nameInputRef = useRef();
+  const photoUrlRef = useRef();
+
+  const autogetData = async () => {
+    const token = localStorage.getItem("Token");
+    try {
+      const res = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:lookup?key=AIzaSyA_u3j-_CtI_i8U5vWkP9qADXUZaJIU1AI",
+        {
+          method: "POST",
+          body: JSON.stringify({
+            idToken: token,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      if (res.ok) {
+        const data = await res.json();
+        data.users.forEach((element) => {
+          console.log(data.users);
+          nameInputRef.current.value = element.displayName;
+          photoUrlRef.current.value = element.photoUrl;
+        });
+      } else {
+        const data = await res.json();
+        console.log(data);
+      }
+    } catch (err) {
+      console.log("Auto fetch error!");
+    }
+  };
+
+  useEffect(() => {
+    autogetData();
+  }, []);
+
+  const updateHandler = async (event) => {
     event.preventDefault();
     const entertedName = nameInputRef.current.value;
     const entertedPhotoUrl = photoUrlRef.current.value;
-    const token = localStorage.getItem('Token');
+    const token = localStorage.getItem("Token");
 
-
-    try{
-        const res = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyA_u3j-_CtI_i8U5vWkP9qADXUZaJIU1AI',
+    try {
+      const res = await fetch(
+        "https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyA_u3j-_CtI_i8U5vWkP9qADXUZaJIU1AI",
         {
-            method: "POST",
-            body: JSON.stringify({
-                idToken: token,
-                displayName: entertedName,
-                photoUrl: entertedPhotoUrl,
-              returnSecureToken: true,
-            }),
-            headers: {
-              "Content-Type": "application/json",
-            },
-          }
-        )
-        if(res.ok){
-            const data = await res.json();
-            console.log('Updated success');
-            nameInputRef.current.value='';
-            photoUrlRef.current.value='';
-        }else{
-            const data = await res.json();
-            alert(data.error.message)
+          method: "POST",
+          body: JSON.stringify({
+            idToken: token,
+            displayName: entertedName,
+            photoUrl: entertedPhotoUrl,
+            returnSecureToken: true,
+          }),
+          headers: {
+            "Content-Type": "application/json",
+          },
         }
-    }catch(err){
-        console('Updaing went wrong!')
+      );
+      if (res.ok) {
+        const data = await res.json();
+        console.log("Updated success");
+        nameInputRef.current.value = "";
+        photoUrlRef.current.value = "";
+      } else {
+        const data = await res.json();
+        alert(data.error.message);
+      }
+    } catch (err) {
+      console("Updating went wrong!");
     }
-
-
   };
   return (
     <div className="profileupdate">
       <h2>Contact Details</h2>
-      <form >
+      <form>
         <div className="profiledetails">
           <label>Full Name</label>
           <input type="text" ref={nameInputRef} required />
@@ -54,7 +89,9 @@ const ProfileForm = () => {
           <input type="text" ref={photoUrlRef} required />
         </div>
         <div className="btn">
-          <button onClick={updateHandler} className="updatebtn">Update</button>
+          <button onClick={updateHandler} className="updatebtn">
+            Update
+          </button>
         </div>
       </form>
     </div>
